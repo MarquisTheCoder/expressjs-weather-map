1f774608a5391f85e06822ee81cca2ca00191e0e
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-11-06 18:42:39 -0600

    showcase image

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/public'));
 
+//handles request and response of website
 app.get('/', (req, res) =>{
      console.log('get request recieved')
      res.status(200).render('index')
